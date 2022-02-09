d37ad3397e118b62afdb744121d7db15cae50497
Максим Алехин <Max-crf@mail.ru>
2022-02-09 18:37:27 +0700

    Добавьте файлы проекта.

---
+++ WpfЗадание 4. Содержимое/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfЗадание_4.Содержимое
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
